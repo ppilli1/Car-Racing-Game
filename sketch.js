fc613c41fac824e8df401f91c889e0ec7708c6fc
starninjaz8055 <67479423+starninjaz8055@users.noreply.github.com>
2020-09-23 12:55:06 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var database;
+var gameState = 0;
+var playerCount;
+var form;
+var player;
+var game;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    /*ball = createSprite(0,0,10,10);
+    ball.shapeColor = "red";
+    var ballref = database.ref("ball/position");
+    ballref.on("value",readPosition)*/
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+    /*if(position != undefined){
+
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+    }*/
+}
+
+/*function readPosition(data){
+    console.log(data.val())
+    position = data.val()
+    ball.x = position.x
+    ball.y = position.y
+}
+
+function changePosition(x,y){
+    database.ref("ball/position").update({
+        "x": position.x + x,
+        "y": position.y + y
+    })
+}*/
